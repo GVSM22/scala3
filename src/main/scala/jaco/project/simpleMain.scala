1a7bf89638f61ed0047ea8757d917251e4c11bb1
Giacomo Magri <giacomomagri@hotmail.com>
2024-05-16 00:55:13 -0300

    wip - tentando acumular input

--- src/main/scala/jaco/project/simpleMain.scala
+++ src/main/scala/jaco/project/simpleMain.scala
@@ -1,7 +1,10 @@
 package jaco.project
 
-import cats.implicits.*
+import cats.data.State
+
+import scala.concurrent.duration.DurationInt
 import cats.effect.{IO, IOApp}
+import fs2.Stream
 import jaco.project.client.GithubClient
 import jaco.project.handler.{InputHandler, OutputHandler}
 import jaco.project.validation.InputValidation
@@ -14,17 +17,24 @@ object simpleMain extends IOApp.Simple:
   override def run: IO[Unit] =
     val cmd = IO.consoleForIO
 
-    EmberClientBuilder.default[IO]
-      .build
-      .use { client =>
-        val githubClient = GithubClient(client)
-        val validator: Validator[ErrorOr] = InputValidation.validate[ErrorOr]
-        val output = OutputHandler.outputSatanCall[IO]
-          .orElse(OutputHandler.outputSatan[IO])
-          .orElse(OutputHandler.finishProgram[IO])
-          .orElse(OutputHandler.outputGithub(githubClient))
+//    EmberClientBuilder.default[IO]
+//      .build
+//      .use { client =>
+//        val githubClient = GithubClient(client)
+//        val validator: Validator[ErrorOr] = InputValidation.validate[ErrorOr]
+//        val output = OutputHandler.outputSatanCall[IO]
+//          .orElse(OutputHandler.outputSatan[IO])
+//          .orElse(OutputHandler.finishProgram[IO])
+//          .orElse(OutputHandler.outputGithub(githubClient))
+//
+//        InputHandler
+//          .readUntilValid[IO](OutputMessages.greetMessage, validator)
+//          .flatMap(output)
+//      }
 
-        InputHandler
-          .readUntilValid[IO](OutputMessages.greetMessage, validator)
-          .flatMap(output)
-      }
+    
+    val list = scala.collection.mutable.ListBuffer.empty[String]
+    IO.race(IO.consoleForIO.readLine.map(list.addOne), IO.sleep(1.second))
+      .iterateWhile(_.isLeft)
+      .map(_ => list)
+      .flatMap(IO.println)
