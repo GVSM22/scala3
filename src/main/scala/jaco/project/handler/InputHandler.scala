610ae6ed875384024bac0a4ef3be69e8eccc9c63
Giacomo Magri <giacomomagri@hotmail.com>
2024-06-12 00:20:06 -0300

    playing with fs2

--- src/main/scala/jaco/project/handler/InputHandler.scala
+++ src/main/scala/jaco/project/handler/InputHandler.scala
@@ -1,12 +1,17 @@
 package jaco.project.handler
 
+import cats.effect.Temporal
 import cats.effect.std.Console
-import cats.syntax.EitherOps
+import cats.syntax.all.catsSyntaxEither
+import cats.syntax.functor.toFunctorOps
 import cats.{Apply, FlatMap, Functor, Show}
+import fs2.Chunk
 import jaco.project.OutputMessages.failureMsg
 import jaco.project.error.InputError
 import jaco.project.simpleMain.ErrorOr
 import jaco.project.validation.InputValidation.Validator
+
+import scala.concurrent.duration.Duration
 
 object InputHandler:
 
@@ -14,9 +19,18 @@ object InputHandler:
     Apply[F].productR(Console[F].println(msg))(Console[F].readLine)
 
   def readUntilValid[F[_] : FlatMap : Console](msg: String, validator: Validator[ErrorOr]): F[OutputPath] =
-    FlatMap[F].tailRecM(greetAndRead(msg))(Functor[F].map(_)( msgInput =>
-      EitherOps[Throwable, OutputPath](validator(msgInput)).leftMap {
+    FlatMap[F].tailRecM(greetAndRead(msg))(
+      _.map(validator(_).leftMap {
         case err: InputError => greetAndRead(failureMsg(err))
         case _ => greetAndRead(failureMsg("aconteceu alguma loucura, mas não explodi"))
-      }
-    ))
+      })
+    )
+
+  private def readLineToChunk[F[_] : Console : Functor](): F[Chunk[String]] =
+    Console[F].readLine.map(Chunk(_))
+
+  def readUntilTimeout[F[_] : Console : FlatMap : Temporal](timeout: Duration): F[Chunk[String]] =
+    FlatMap[F].tailRecM(Chunk.empty[String]) { chunks =>
+      val timeoutWithChunks = Temporal[F].sleep(timeout).as(chunks)
+      Temporal[F].race(readLineToChunk().map(_ ++ chunks), timeoutWithChunks)
+    }
--- src/main/scala/jaco/project/simpleMain.scala
+++ src/main/scala/jaco/project/simpleMain.scala
@@ -1,40 +1,51 @@
 package jaco.project
 
-import cats.data.State
-
-import scala.concurrent.duration.DurationInt
 import cats.effect.{IO, IOApp}
-import fs2.Stream
+import fs2.{Pipe, Stream}
 import jaco.project.client.GithubClient
 import jaco.project.handler.{InputHandler, OutputHandler}
 import jaco.project.validation.InputValidation
-import jaco.project.validation.InputValidation.Validator
 import org.http4s.ember.client.EmberClientBuilder
+
+import scala.concurrent.duration.DurationInt
 
 object simpleMain extends IOApp.Simple:
   type ErrorOr[T] = Either[Throwable, T]
 
+  private val bizarreCasePipe: Pipe[IO, String, Unit] = _.filter(_.toIntOption.isEmpty)
+    .map { str =>
+    str.dropRight(1).foldRight(str.last.toString)((c, s) => s.appended(if (s.last.isUpper) c.toLower else c.toUpper) )
+  }.printlns
+
   override def run: IO[Unit] =
     val cmd = IO.consoleForIO
 
-//    EmberClientBuilder.default[IO]
-//      .build
-//      .use { client =>
-//        val githubClient = GithubClient(client)
-//        val validator: Validator[ErrorOr] = InputValidation.validate[ErrorOr]
-//        val output = OutputHandler.outputSatanCall[IO]
-//          .orElse(OutputHandler.outputSatan[IO])
-//          .orElse(OutputHandler.finishProgram[IO])
-//          .orElse(OutputHandler.outputGithub(githubClient))
-//
-//        InputHandler
-//          .readUntilValid[IO](OutputMessages.greetMessage, validator)
-//          .flatMap(output)
-//      }
+    EmberClientBuilder.default[IO]
+      .build
+      .use { client =>
+        val githubClient = GithubClient(client)
+        val validator = InputValidation.validate[IO]
+        val output = OutputHandler.outputSatanCall[IO]
+          .orElse(OutputHandler.outputSatan[IO])
+          .orElse(OutputHandler.finishProgram[IO])
+          .orElse(OutputHandler.outputGithub(githubClient))
 
-    
-    val list = scala.collection.mutable.ListBuffer.empty[String]
-    IO.race(IO.consoleForIO.readLine.map(list.addOne), IO.sleep(1.second))
-      .iterateWhile(_.isLeft)
-      .map(_ => list)
-      .flatMap(IO.println)
+        val outputPipe: Pipe[IO, String, Unit] =
+          _.evalMap(
+            validator(_)
+              .flatMap(output)
+              .voidError
+          )
+
+        InputHandler.readUntilTimeout[IO](3.seconds)
+          .map(Stream.chunk)
+          .flatMap {
+            _.covary[IO]
+              .broadcastThrough(
+              bizarreCasePipe(_).printlns,
+              outputPipe
+            )
+              .compile
+              .drain
+          }
+      }
